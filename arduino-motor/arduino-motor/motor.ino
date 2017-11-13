2c61e5e2e55164aeb35a6d8654b7e085b9f5840c
Hieu Nguyen <hnguye19@hawk.iit.edu>
2017-11-12 19:00:10 -0800

    create a new motor solution

---
+++ arduino-motor/arduino-motor/motor.ino
@@ -0,0 +1,6 @@
+﻿void setup()
+{
+}
+void loop()
+{
+}
---
+++ arduino-motor/test/motor.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		motor.ino
+ Created:	11/12/2017 6:57:40 PM
+ Author:	Hieu Nguyen
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
