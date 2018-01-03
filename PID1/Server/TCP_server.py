dff257a235f01598b4a9279ea389b4a1ce822b7c
Louis Lim <louis_lim97@outlook.com>
2018-01-02 20:11:28 -0600

    Updates: 1. Fixed bugs in Arduino.py ( Able to recover from crashing) 2. Listen(1) in TCP class

--- PID1/Server/TCP_server.py
+++ PID1/Server/TCP_server.py
@@ -1,14 +1,22 @@
 from tcp import Controller
 from time import sleep
+from arduino import Arduino
+from timer import Timer
+import RPi.GPIO as GPIO
+from queue import Queue
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-pc = Controller("192.168.2.100")
+q = Queue()
+pc = Controller("192.168.2.104")
+arduino = Arduino(i2cAddress = 7)
 pc.start()
+timer = Timer()
+         
 
 while 1:
     commands = pc.getNewCommands()
     if  commands is not None:
         for command in commands:
-            print(command)
-
-
-
+            if arduino.write(1, list(command)) is False:
+                print("Failed at",command)
--- PID1/Server/arduino.py
+++ PID1/Server/arduino.py
@@ -1,5 +1,4 @@
 """
-Arduino Class
 #have to run 'sudo apt-get install python-smbus'
 #in Terminal to install smbus
 import smbus as smbus
@@ -8,6 +7,8 @@ import smbus as smbus
 commands:
 1: drive motors
 """
+
+
 
 import smbus2
 class Arduino(smbus2.SMBus):
@@ -23,7 +24,7 @@ class Arduino(smbus2.SMBus):
         '''
 
         super().__init__(1)
-        self.i2cAddress=i2cAddress  
+        self.i2cAddress=i2cAddress
 
     def write(self, command, data):
         '''
@@ -35,7 +36,17 @@ class Arduino(smbus2.SMBus):
         '''
         if not isinstance(data,list):
             data=[data,]
-        self.write_i2c_block_data(self.i2cAddress, command, data)
+
+        counter = 0
+        while counter < 5:
+            try:
+                self.write_i2c_block_data(self.i2cAddress, command, data)               
+                return True
+            except:
+                #print(command, "failed")
+                counter+=1
+        return False
+            
         #time.sleep(0.01)#must delay 0.01 second
 
     def read(self, numBytes):
@@ -47,12 +58,13 @@ class Arduino(smbus2.SMBus):
         '''
         return self.read_i2c_block_data(self.i2cAddress, 0, numBytes)
 
-    def motor(self, motorID, power):
+    def motor(self, device, value):
         '''
         Send a drive command to the arduino via i2c connection.
 
         Args:
             motorID: the motor to be driven. Values: 1, 2, 3, 4, ...
-            power: power and direction of the motor. Values: [-127, 127]
+            power: power and dirrection of the motor. Values: [-127, 127]
         '''
-        self.write(1, [motorID, power])
+        
+        return self.write(1, [1, device, value])
