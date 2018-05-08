42c94522a66355ba8e987958aeec4b93b84062c8
RilesScott <32188086+RilesScott@users.noreply.github.com>
2018-05-08 05:38:09 -0500

    Update clive.py

--- clive.py
+++ clive.py
@@ -8,19 +8,19 @@ class Pathfinding:
 
     def __init__(self):
         self.isForward = True
-        self.r = 0.0  # radius of robot
+        self.r = 500.0  # radius of robot
         self.rX = 0.0  # x-coordinate of robot
         self.rY = 0.0  # y-coordinate of robot
 
-        self.obstacles = [[0.0, 0.0, 0.0],
-                          [0.0, 0.0, 0.0],
-                          [0.0, 0.0, 0.0],
-                          [0.0, 0.0, 0.0],
-                          [0.0, 0.0, 0.0]]  # holds upto 5 self.obstacles
+        self.obstacles = [[0.0, 0.0],
+                          [0.0, 0.0],
+                          [0.0, 0.0],
+                          [0.0, 0.0],
+                          [0.0, 0.0]]  # holds upto 5 self.obstacles
 
         self.numPivots = 0  # num of relevant self.obstacles
-        self.pivots = [[0.0, 0.0, 0.0],
-                       [0.0, 0.0, 0.0]]  # works with only 2 self.obstacles at any time
+        self.pivots = [[0.0, 0.0],
+                       [0.0, 0.0]]  # works with only 2 self.obstacles at any time
 
         self.dirr = 1
         self.dest = 1
@@ -128,7 +128,7 @@ class Pathfinding:
         self.yA = self.pivots[0][1]
         self.xB = self.pivots[1][0]
         self.yB = self.pivots[1][1]
-        width = math.sqrt(math.pow(self.xB - self.xA, 2) + math.pow(self.yB - self.yA, 2)) - self.pivots[0][2] - self.pivots[1][2]
+        width = math.sqrt(math.pow(self.xB - self.xA, 2) + math.pow(self.yB - self.yA, 2)) - 300
         self.x1 = (self.xA + self.xB) / 2
         self.y1 = (self.yA + self.yB) / 2
         if (self.isForward == True):
@@ -171,8 +171,9 @@ class Pathfinding:
         self.rX = robotLoc[0]
         self.rY = robotLoc[1]
         self.numPivots = numPivots
-        self.pivots = obstacles
-        # pivots = closet 2 obstacles
+        self.obstacles = obstacles
+        self.filter()
+        self.getPivots()
         pass
 
 
@@ -185,8 +186,70 @@ class Pathfinding:
             self.pathB()
         else:
             pass
+        #self.check() # Allows for obstacles in 1.5m radius only
         return [(self.x1, self.y1), (self.x2, self.y2), (self.x3, self.y3)]
 
+    def check(self):
+        self.dA = math.sqrt(math.pow(self.xA - self.rX, 2) + math.pow(self.yA - self.rY, 2))
+        self.dB = math.sqrt(math.pow(self.xB - self.rX, 2) + math.pow(self.yB - self.rY, 2))
+        if(self.dA > 1500 or self.dB > 1500):
+            self.moveStraight()
+
+    def getPivots(self):
+        d = [[0.0, 0.0],
+             [0.0, 0.0],
+             [0.0, 0.0],
+             [0.0, 0.0],
+             [0.0, 0.0]]
+        if self.numPivots > 2:
+            min = 100000
+            minI = 100
+            for i in range(self.numPivots):
+                d[i] = math.sqrt(math.pow(self.obstacles[i][0] - self.rX, 2) + math.pow(self.obstacles[i][1] - self.rY, 2))
+                if d[i] < min:
+                    minI = i
+            self.pivots[0] = (self.obstacles[minI][0], self.obstacles[minI][1])
+            del self.obstacles[minI]
+            self.numPivots -= 1
+            min = 100000
+            minI = 100
+            for i in range(self.numPivots):
+                d[i] = math.sqrt(math.pow(self.obstacles[i][0] - self.rX, 2) + math.pow(self.obstacles[i][1] - self.rY, 2))
+                if d[i] < min:
+                    minI = i
+            self.pivots[1] = (self.obstacles[minI][0], self.obstacles[minI][1])
+            self.numPivots = 2
+
+        elif self.numPivots == 2:
+            min = 100000
+            minI = 100
+            for i in range(2):
+                d[i] = math.sqrt(math.pow(self.obstacles[i][0] - self.rX, 2) + math.pow(self.obstacles[i][1] - self.rY, 2))
+                if d[i] < min:
+                    minI = i
+            self.pivots[0] = (self.obstacles[minI][0], self.obstacles[minI][1])
+            del self.obstacles[minI]
+            self.pivots[1] = (self.obstacles[0][0], self.obstacles[0][1])
+
+        elif self.numPivots == 1:
+            self.pivots[0] = (self.obstacles[0][0], self.obstacles[0][1])
+
+    def filter(self):
+        if(self.numPivots > 5):
+            t = self.numPivots - 5
+            for i in range(t):
+                del self.obstacles[5]
+
+        temp = self.obstacles
+        loc = 0
+        for i in range(self.numPivots):
+            if (self.obstacles[i][0] > self.rX):
+                temp.insert(loc, [self.obstacles[i]])
+                loc += 1
+        for i in range(self.numPivots):
+            del temp[0]
+        self.numPivots = loc
+        self.obstacles = temp
 #
 # End Pathfinding
 #
